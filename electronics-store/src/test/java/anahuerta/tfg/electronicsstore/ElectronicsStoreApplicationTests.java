3e862c9266da73f7926be870ddd509c09c3b4811
anahuertat <anahuerta221@gmail.com>
2020-01-22 18:42:09 +0100

    Primer commit. Archivos generados por spring initializr

---
+++ electronics-store/src/test/java/anahuerta/tfg/electronicsstore/ElectronicsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package anahuerta.tfg.electronicsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
